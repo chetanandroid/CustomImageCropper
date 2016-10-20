1643db5d5285b622a98565a06c32f8333a20d9b0
Ajinkya <ajinkya@sphinx-solution.com>
2016-10-20 14:51:32 +0530

    Added Marshmallow Runtime Permissions

--- app/src/main/java/com/simplecropimage/cs/MainActivity.java
+++ app/src/main/java/com/simplecropimage/cs/MainActivity.java
@@ -1,7 +1,9 @@
 package com.simplecropimage.cs;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
@@ -9,13 +11,20 @@ import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageView imageViewMain;
-    private TextView txtViewOpenDialog;
+    private Button btnOpenDialog;
     private int REQUEST_CAPTURE_IMAGE = 1, REQUEST_PICK_IMAGE = 2, REQUEST_CROP_IMAGE = 3;
+    private static final int REQUEST_CODE_APP_PERMISSIONS = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +36,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void initViews() {
         imageViewMain = (ImageView) findViewById(R.id.imageViewMain);
-        txtViewOpenDialog = (TextView) findViewById(R.id.txtViewOpenDialog);
+        btnOpenDialog = (Button) findViewById(R.id.btnOpenDialog);
     }
 
     private void setUpViews() {
-        txtViewOpenDialog.setOnClickListener(this);
+        btnOpenDialog.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.txtViewOpenDialog:
-                selectImage();
+            case R.id.btnOpenDialog:
+                //checking runtime permission for marshmallow and higher versions
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    checkRuntimePermissions();
+                } else {
+                    selectImage();
+                }
                 break;
         }
     }
@@ -83,10 +97,88 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Uri uri = result.getData();
             Intent intent = new Intent(MainActivity.this, CropActivity.class);
             intent.setData(uri);
-            //intent.putExtra("crop-type","circle");
+            //Specify the crop view type using intent -- if you don't set crop type then square is default
+            //intent.putExtra("crop-type","square");
             startActivityForResult(intent, REQUEST_CROP_IMAGE);
         } else if (requestCode == REQUEST_CROP_IMAGE && resultCode == RESULT_OK) {
             imageViewMain.setImageBitmap(BitmapFactory.decodeFile(result.getStringExtra("cropped-image-path")));
         }
     }
+
+    /*****
+    * All The code below checks Runtime Permissions for Andoid Marshmallow and Higher
+    ***/
+    private void checkRuntimePermissions() {
+        List<String> permissionsNeeded = new ArrayList<String>();
+
+        final List<String> permissionsList = new ArrayList<String>();
+        if (!addPermission(permissionsList, Manifest.permission.CAMERA))
+            permissionsNeeded.add(getString(R.string.camera));
+        if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            permissionsNeeded.add(getString(R.string.write_storage_permission));
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = getString(R.string.you_need_to_grant_access_to) + permissionsNeeded.get(0);
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+                showMessageOKCancel(message, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), REQUEST_CODE_APP_PERMISSIONS);
+                    }
+                });
+                return;
+            }
+            requestPermissions(permissionsList.toArray(new String[permissionsList.size()]), REQUEST_CODE_APP_PERMISSIONS);
+            return;
+        }
+        selectImage();
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!shouldShowRequestPermissionRationale(permission))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_APP_PERMISSIONS: {
+                Map<String, Integer> perms = new HashMap<String, Integer>();
+                // Initial
+                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for CAMERA & WRITE_EXTERNAL_STORAGE permission
+                if (perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // All Permissions Granted
+                    selectImage();
+                } else {
+                    //Some Permission Denied
+                    Toast.makeText(MainActivity.this, getString(R.string.permission_required_by_application_denied), Toast.LENGTH_SHORT).show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.ok), okListener)
+                .setNegativeButton(getString(R.string.cancel), null)
+                .create()
+                .show();
+    }
+    /***** Permission check code ends here *****/
 }
